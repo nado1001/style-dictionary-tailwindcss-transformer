8f9de855d18fcead1f320d0db6b095ea8b28ba83
nado1001 <tk.tech1001@gmail.com>
2022-08-11 16:34:13 +0900

    chore: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,23 @@
 module.exports = {
   env: { es6: true, node: true, jest: true },
-  plugins: [
-    'import'
-  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: './tsconfig.json' },
+  plugins: ['import'],
   extends: [
+    'plugin:@typescript-eslint/recommended',
     'eslint:recommended',
     'prettier'
   ],
   rules: {
     'no-console': ['warn', { allow: ['warn', 'info', 'error'] }],
+    'prefer-const': 'error',
+
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/explicit-module-boundary-types': ['off'],
+    '@typescript-eslint/no-empty-function': ['off'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    'import/prefer-default-export': ['off'],
+    '@typescript-eslint/ban-types': ['warn']
   }
 }
