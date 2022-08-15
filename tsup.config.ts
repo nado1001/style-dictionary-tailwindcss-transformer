f24778916783818957b8ce509d90ebd16fb9cd17
nado1001 <tk.tech1001@gmail.com>
2022-08-15 10:53:39 +0900

    chore: update tsup.config.json

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from 'tsup'
 
 const isProd = process.env.NODE_ENV === 'production'
 
-export default defineConfig([
-  {
-    entry: ['src/index.ts'],
-    clean: true,
-    watch: !isProd,
-    format: ['cjs'],
-    minify: isProd,
-    treeshake: isProd,
-    sourcemap: !isProd
-  }
-])
+export default defineConfig({
+  entry: ['src/index.ts'],
+  clean: true,
+  watch: !isProd,
+  format: ['cjs'],
+  minify: isProd,
+  treeshake: isProd,
+  sourcemap: !isProd
+})
