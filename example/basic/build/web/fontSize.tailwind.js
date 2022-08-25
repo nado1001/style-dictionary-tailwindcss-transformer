2af7fc64ded2c94bcc8d0127c9eb5c9e659a1de1
nado1001 <tk.tech1001@gmail.com>
2022-08-26 01:02:11 +0900

    chore: update examples

--- example/basic/build/web/fontSize.tailwind.js
+++ example/basic/build/web/fontSize.tailwind.js
@@ -1,6 +1,6 @@
 module.exports = {
-	small: "0.75rem",
-	medium: "1rem",
-	large: "2rem",
-	base: "1rem"
+  small: "0.75px",
+  medium: "1px",
+  large: "2px",
+  base: "1px"
 }
--- example/basic/sd.config.js
+++ example/basic/sd.config.js
@@ -1,55 +1,31 @@
 const StyleDictionaryModule = require('style-dictionary')
 const { sdTailwindConfig } = require('../../dist')
 
-let StyleDictionary = StyleDictionaryModule.extend({
-  source: [`tokens/**/*.json`],
-  format: {},
-  platforms: {
-    scss: {
-      transformGroup: 'scss',
-      buildPath: 'build/scss/',
-      files: [
-        {
-          destination: '_tokens.scss',
-          format: 'scss/variables',
-          options: {
-            outputReferences: true
-          }
-        }
-      ]
-    },
-    css: {
-      transformGroup: 'css',
-      buildPath: 'build/css/',
-      files: [
-        {
-          destination: '_tokens.css',
-          format: 'css/variables',
-          options: {
-            outputReferences: true
-          }
-        }
-      ]
+StyleDictionaryModule.registerTransform({
+  type: `value`,
+  transitive: true,
+  name: `tailwind/font/px`,
+  matcher: function (token) {
+    return token.attributes.category === 'fontSize'
+  },
+  transformer: function ({ value }) {
+    if (value.indexOf('px') !== -1) {
+      return value
     }
+
+    return `${value}px`
   }
 })
-
-StyleDictionary.buildAllPlatforms()
 
 const types = ['colors', 'fontSize']
 
 types.map((type) => {
-  console.log('\n==============================================')
-  console.log(`\nProcessing: [${type}]`)
-
-  StyleDictionary = StyleDictionaryModule.extend(
+  const StyleDictionary = StyleDictionaryModule.extend(
     sdTailwindConfig({
-      type
+      type,
+      transforms: ['attribute/cti', 'name/cti/kebab', 'tailwind/font/px']
     })
   )
 
-  // StyleDictionary.cleanAllPlatforms()
   StyleDictionary.buildAllPlatforms()
-
-  console.log('\nEnd processing')
 })
