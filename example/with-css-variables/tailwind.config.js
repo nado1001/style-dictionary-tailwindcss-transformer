59954554721ed87fd8d195407eac2dd424ca6f38
nado1001 <tk.tech1001@gmail.com>
2022-09-09 01:32:12 +0900

    chore: update examples

--- example/with-css-variables/tailwind.config.js
+++ example/with-css-variables/tailwind.config.js
@@ -1,4 +1,3 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
@@ -30,4 +29,5 @@ module.exports = {
       }
     },
   },
+  plugins: [require("@tailwindcss/typography"),require("@tailwindcss/forms")]
 }
